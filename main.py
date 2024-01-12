66ae01758353dd3f35fee6c267a7238858250248
Hưng Tuấn Nguyễn <91263297+hunter1405@users.noreply.github.com>
2024-01-12 12:06:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 # Mock function to simulate getting stock data
