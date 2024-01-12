b515d0c934edb60fb41b119a201d832bf749263b
Hưng Tuấn Nguyễn <91263297+hunter1405@users.noreply.github.com>
2024-01-12 15:45:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+import pandas_datareader.data as web
 import streamlit as st
 import pandas as pd
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
-import pandas_datareader.data as web
+import yfinance as yf
 from datetime import datetime
-import yfinance
 
-# Function to fetch stock data using pandas_datareader
-def fetch_stock_data(stock_code, start_date, end_date, data_type='Close'):
-    try:
-        stock_data = web.DataReader(stock_code, 'yahoo', start_date, end_date)
-        print(stock_data)  # Debug: Print the raw data
-        return stock_data[data_type]
-    except Exception as e:
-        st.error(f"Error fetching stock data: {e}")
-        return pd.DataFrame()  # Return an empty DataFrame on error
+# Function to fetch stock data using yfinance
+def fetch_stock_data(stock_code, start_date, end_date):
+    stock_data = yf.download(stock_code, start=start_date, end=end_date)
+    return stock_data
+
+# Streamlit UI for Data fetching
+st.title('Stock Forecasting Application')
+st.subheader('Fetch Stock Data')
+input_stock_code = st.text_input('Enter Stock Ticker', 'AAPL', key='input_stock_code').upper()
+start_date = st.date_input('Start Date', datetime(2021, 1, 1), key='start_date')
+end_date = st.date_input('End Date', datetime.today(), key='end_date')
+
+# Fetch and display the data
+if st.button('Fetch Data', key='fetch_data'):
+    data = fetch_stock_data(input_stock_code, start_date, end_date)
+    st.write(data.head())  # Display the first few rows of the data
+    csv = data.to_csv().encode('utf-8')
+    st.download_button(label="Download data as CSV", data=csv, file_name=f'{input_stock_code}.csv', mime='text/csv')
 
 # Functions for forecasting models
 def moving_average(series, window):
